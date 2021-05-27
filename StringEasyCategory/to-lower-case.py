d66244338276a5f0de9a9424e3f1c78472ee41a4
VarunKuppanahalli <49811466+varunkr95g@users.noreply.github.com>
2021-05-27 16:44:50 +0530

    Create to-lower-case.py

---
+++ StringEasyCategory/to-lower-case.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        str_lower=str.lower()
+
+        return str_lower
