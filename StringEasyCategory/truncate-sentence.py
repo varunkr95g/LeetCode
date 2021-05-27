0b2aece9f3135863bb0c424983ab6c7cadef2f72
VarunKuppanahalli <49811466+varunkr95g@users.noreply.github.com>
2021-05-27 16:51:32 +0530

    Create truncate-sentence.py

---
+++ StringEasyCategory/truncate-sentence.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def truncateSentence(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        
+        lists=s.split(" ")
+
+        return ' '.join(lists[:k])
