c39d62fe75b3b19fee42bac5d852a10249380721
VarunKuppanahalli <49811466+varunkr95g@users.noreply.github.com>
2021-05-30 18:59:15 +0530

    Create intersection-of-two-arrays-ii.py

---
+++ HashTableEasyCategory/intersection-of-two-arrays-ii.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        length1=len(nums1)
+        length2=len(nums2)
+        sub_arr=[]
+        sub_arr_2=[]
+        
+        if length1 > length2:
+            for i in range(len(nums2)):
+                if( nums2[i] in nums1):
+                    sub_arr.append(nums2[i])
+                    nums1.remove(nums2[i])
+        else:
+            for i in range(len(nums1)):
+                if (nums1[i] in nums2):
+                    sub_arr.append(nums1[i])
+                    nums2.remove(nums1[i])
+        
+        
+        # sub_arr_2=list(set(sub_arr))
+        
+        return sub_arr
