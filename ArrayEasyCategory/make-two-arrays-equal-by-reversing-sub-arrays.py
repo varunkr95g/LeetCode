e73a5e3ae503487a63240268c2e81e91444011d7
VarunKuppanahalli <49811466+varunkr95g@users.noreply.github.com>
2021-05-30 15:16:07 +0530

    Create make-two-arrays-equal-by-reversing-sub-arrays.py

---
+++ ArrayEasyCategory/make-two-arrays-equal-by-reversing-sub-arrays.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canBeEqual(self, target, arr):
+        """
+        :type target: List[int]
+        :type arr: List[int]
+        :rtype: bool
+        """
+        target.sort()
+        arr.sort()
+        
+        if(target==arr):
+            return True
+        else:
+            return False
