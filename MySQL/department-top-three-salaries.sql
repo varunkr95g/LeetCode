b3040415d597678ba78b4624b997abf09239095c
VarunKuppanahalli <49811466+varunkr95g@users.noreply.github.com>
2021-11-05 11:54:08 +0530

    Create department-top-three-salaries.sql

---
+++ MySQL/department-top-three-salaries.sql
@@ -0,0 +1,8 @@
+select cnt67.Department,cnt67.Employee,cnt67.Salary from
+(
+select Emp.Name as Employee,Emp.Salary,Dept.Name as Department,dense_rank() 
+over(partition by Emp.DepartmentId order by Salary desc ) as dr
+from Employee Emp left outer join Department Dept
+on Emp.DepartmentId=Dept.Id
+) cnt67
+where cnt67.dr<=3
